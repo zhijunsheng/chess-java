b6a06e34478cce6f12693f334a4d241a2d035ab2
zhijunsheng <zhijunsheng@localhost>
2020-12-02 11:56:02 -0500

    deployed initial 32 chess pieces

--- Chess/src/com/goldthumb/chess/ChessModel.java
+++ Chess/src/com/goldthumb/chess/ChessModel.java
@@ -7,7 +7,27 @@ public class ChessModel {
 	private Set<ChessPiece> piecesBox = new HashSet<ChessPiece>();
 	
 	void reset() {
-		piecesBox.add(new ChessPiece(0, 7, Player.BLACK, Rank.ROOK, "Rook-black"));
+		for (int i = 0; i < 2; i++) {
+			piecesBox.add(new ChessPiece(0 + i * 7, 7, Player.BLACK, Rank.ROOK, "Rook-black"));
+			piecesBox.add(new ChessPiece(0 + i * 7, 0, Player.WHITE, Rank.ROOK, "Rook-white"));
+
+			piecesBox.add(new ChessPiece(1 + i * 5, 7, Player.BLACK, Rank.KNIGHT, "Knight-black"));
+			piecesBox.add(new ChessPiece(1 + i * 5, 0, Player.WHITE, Rank.KNIGHT, "Knight-white"));
+
+			piecesBox.add(new ChessPiece(2 + i * 3, 7, Player.BLACK, Rank.BISHOP, "Bishop-black"));
+			piecesBox.add(new ChessPiece(2 + i * 3, 0, Player.WHITE, Rank.BISHOP, "Bishop-white"));
+		}
+		
+		for (int i = 0; i < 8; i++) {
+			piecesBox.add(new ChessPiece(i, 6, Player.BLACK, Rank.PAWN, "Pawn-black"));
+			piecesBox.add(new ChessPiece(i, 1, Player.WHITE, Rank.PAWN, "Pawn-white"));
+		}
+		
+		piecesBox.add(new ChessPiece(3, 7, Player.BLACK, Rank.QUEEN, "Queen-black"));
+		piecesBox.add(new ChessPiece(3, 0, Player.WHITE, Rank.QUEEN, "Queen-white"));
+		piecesBox.add(new ChessPiece(4, 7, Player.BLACK, Rank.KING, "King-black"));
+		piecesBox.add(new ChessPiece(4, 0, Player.WHITE, Rank.KING, "King-white"));
+		
 	}
 	
 	ChessPiece pieceAt(int col, int row) {
@@ -30,7 +50,27 @@ public class ChessModel {
 				if (p == null) {
 					desc += " .";
 				} else {
-					desc += " R";
+					desc += " ";
+					switch (p.rank) {
+					case KING: 
+						desc += p.player == Player.WHITE ? "k" : "K";
+						break;
+					case QUEEN: 
+						desc += p.player == Player.WHITE ? "q" : "Q";
+						break;
+					case BISHOP: 
+						desc += p.player == Player.WHITE ? "b" : "B";
+						break;
+					case ROOK: 
+						desc += p.player == Player.WHITE ? "r" : "R";
+						break;
+					case KNIGHT: 
+						desc += p.player == Player.WHITE ? "n" : "N";
+						break;
+					case PAWN: 
+						desc += p.player == Player.WHITE ? "p" : "P";
+						break;
+					}
 				}
 			}
 			desc += "\n";
