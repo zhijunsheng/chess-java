90f418475a5f1ecfb35e70ed78d9aae039369a63
zhijunsheng <zhijunsheng@localhost>
2020-12-02 10:50:13 -0500

    implemented ChessModel.toString for empty chess board

--- Chess/src/com/goldthumb/chess/ChessModel.java
+++ Chess/src/com/goldthumb/chess/ChessModel.java
@@ -2,4 +2,19 @@ package com.goldthumb.chess;
 
 public class ChessModel {
 
+	@Override
+	public String toString() {
+		String desc = "";
+		
+		for (int row = 7; row >= 0; row--) {
+			desc += "" + row;
+			for (int col = 0; col < 8; col++) {
+				desc += " .";
+			}
+			desc += "\n";
+		}
+		desc += "  0 1 2 3 4 5 6 7";
+		
+		return desc;
+	}
 }
---
+++ Chess/tst/com/goldthumb/chess/ChessModelTest.java
@@ -0,0 +1,15 @@
+package com.goldthumb.chess;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ChessModelTest {
+
+	@Test
+	void testToString() {
+		ChessModel chessModel = new ChessModel();
+		System.out.println(chessModel);
+	}
+
+}
